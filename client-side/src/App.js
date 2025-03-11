70fd89270f752177c84ba67b178e501060b4f447
Mohan Kumar S <mohanmoku2004@gamil.com>
2025-03-11 20:42:20 +0530

    backend init

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-blue-500">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
