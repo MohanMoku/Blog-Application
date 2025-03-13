e1d1f221ecf9149a1a7019448986ea75d0ff7ded
Mohan Kumar S <mohanmoku2004@gamil.com>
2025-03-13 22:36:49 +0530

    add dashboard layout with sidebar and profile tab

--- client-side/src/pages/Dashboard.js
+++ client-side/src/pages/Dashboard.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import DasSidebar from '../components/DasSidebar'
+import DashProfile from '../components/DashProfile'
 
 const Dashboard = () => {
+
+  const location = useLocation()
+  const [tab, setTab] = useState('')
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get('tab')
+    if(tabFromUrl === 'profile'){
+      setTab('profile')
+    }
+  }, [location.search])
+
   return (
-    <div>Dashboard</div>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className="md:w-56">
+        {/* sidebar */}
+        <DasSidebar />
+      </div>
+      {/* profile */}
+      {tab === 'profile' && <DashProfile />}
+    </div>
   )
 }
 
