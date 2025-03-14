98019359d36f08915c2ea27bad47f9653c7bdc7c
Mohan Kumar S <mohanmoku2004@gamil.com>
2025-03-14 18:23:19 +0530

    implement profile image upload and user details form in DashProfile component

--- client-side/src/components/DashProfile.js
+++ client-side/src/components/DashProfile.js
@@ -1,7 +1,59 @@
-import React from 'react'
+import { Button, TextInput } from 'flowbite-react'
+import React, { useEffect, useRef, useState } from 'react'
+import { useSelector } from 'react-redux'
 
 export default function DashProfile() {
-  return (
-    <div>DashProfile</div>
-  )
+
+    const { currentUser } = useSelector((state) => state.user)
+    const [imageFile , setImageFile] = useState('')
+    const [imageFileUrl, setImageFileUrl] = useState('')
+    const filePickerRef = useRef()
+
+    const handelImageChange = (e) => {
+        const file = e.target.files[0]
+        if(file){
+            setImageFile(e.target.files[0])
+            setImageFileUrl(URL.createObjectURL(file))
+        }
+    }
+
+    useEffect(()=>{
+        if(imageFile){
+            // uploadImage();
+        }
+    }, [imageFile])
+
+    // const uploadImage = async () => {
+        
+    // }
+
+    return (
+        <div className='max-w-lg mx-auto p-3 w-full'>
+
+            <h1 className='my-5 text-center font-semibold text-3xl'>Profile</h1>
+
+            <form className='flex flex-col gap-4'>
+
+                <input type="file" accept='image/*' onChange={handelImageChange} ref={filePickerRef} hidden/>
+
+                <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" onClick={() => filePickerRef.current.click()}>
+                    <img src={imageFileUrl || currentUser.profilePicture} alt="img" className='rounded-full w-full h-full object-cover border-8 border-[lightgray]' />
+                </div>
+
+                <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} />
+                <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} />
+                <TextInput type='password' id='password' placeholder='password' />
+
+                <Button type='submit' gradientDuoTone='purpleToBlue' outline>
+                    Update
+                </Button>
+
+            </form>
+
+            <div className="text-red-500 flex justify-between mt-5">
+                <span className='cursor-pointer'>Delete Account</span>
+                <span className='cursor-pointer'>Sign Out</span>
+            </div>
+        </div>
+    )
 }
